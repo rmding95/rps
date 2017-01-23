215a74479946a6d04d4de7af2fc9e132927056fa
Richard Ding <rmding95@uw.edu>
2017-01-23 02:30:18 -0800

    started working on matchmaking

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var users = {};
+var rooms = {};
+var queue = [];
 app.use(express.static('static'));
 
 app.get('/', function(req, res) {
@@ -11,10 +14,49 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket) {
     console.log('a user connected');
+
+    socket.on('join', function(name) {
+        users[socket.id] = name;
+        io.emit('update_all', users);
+        matchmake(socket);
+    });
+
+    var clients_in_the_room = io.sockets.adapter.rooms; 
+    console.log(Object.keys(clients_in_the_room).length);
+    io.emit('connected_users', Object.keys(clients_in_the_room).length, users);
+    for (var clientId in clients_in_the_room ) {
+        console.log('client: %s', clientId); //Seeing is believing 
+        var client_socket = io.sockets.connected[clientId];//Do whatever you want with this
+    }
+
     socket.on('disconnect', function() {
         console.log('user disconnected');
+        delete users[socket.id];
+        io.emit('connected_users', Object.keys(clients_in_the_room).length);
+        io.emit('update_all', users);
     });
-})
+
+    socket.on('turn', function(msg) {
+        console.log(msg);
+        io.emit('turn', msg);
+    });
+});
+
+function matchmake(socket) {
+    if (queue.length > 0) {
+        var peer = queue.pop();
+        var room = socket.id + "#" + peer.id;
+        peer.join(room);
+        socket.join(room);
+        rooms[peer.id] = room;
+        rooms[socket.id] = room;
+        peer.emit('game_start', {'name': users[socket.id], 'room': room});
+        socket.emit('game_start', {'name': users[peer.id], 'room': room});
+    } else {
+        queue.push(socket);
+        socket.emit('game_wait');
+    }
+}
 
 http.listen(3000, function() {
     console.log('listening on *:3000');
--- static/socket.js
+++ static/socket.js
@@ -1,4 +1,60 @@
 $(function() {
     var socket = io();
-    console.log("wow");
+    var connected_users;
+    var username = '';
+    var clientId = '';
+
+    socket.on('connected_users', function(connected_users, users) {
+        clientId = socket.io.engine.id;
+        users[clientId] = "Anon#" + connected_users;
+        updateUserList(users);
+        $("#username").text("Username: " + users[clientId]);
+        $("#user_count").text("Users Connected: " + connected_users);
+    });
+
+    $("#find_game").click(function() {
+        username_field = $("#username_input").val();
+        console.log(username_field);
+        if (username_field.length != 0) {
+            username = username_field
+        }
+        socket.emit('join', username);
+    });
+
+    socket.on('update_all', function(msg) {
+        console.log(msg);
+        updateUserList(msg);
+        $("#username").text("Username: " + msg[clientId]);
+    });
+
+    $("#rock").click(function() {
+        console.log("rock");
+        socket.emit('turn', "rock");
+    });
+
+    $("#paper").click(function() {
+        console.log("rock");
+        socket.emit('turn', "paper");
+    });
+
+    $("#scissors").click(function() {
+        console.log("rock");
+        socket.emit('turn', "scissors");
+    });   
+
+    socket.on('turn', function(msg) {
+        $("#log").append($('<li>').text(msg));
+    });
+
+    socket.on('game_wait', function() {
+        $("#log").text("Waiting for game");
+    });
 });
+
+function updateUserList(users) {
+    $("#user_list").empty();
+    for (var key in users) {
+        $("#user_list").append($('<li>').text(users[key]));
+    }
+}
+
